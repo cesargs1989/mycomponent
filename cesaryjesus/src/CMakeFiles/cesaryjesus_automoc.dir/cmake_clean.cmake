349d6b698bff2186e3dba2d428ce536336c3eafd
sergieteja <sergiobs93@gmail.com>
2016-11-08 17:19:05 +0100

    borrar fusca

--- cesaryjesus/src/CMakeFiles/cesaryjesus_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "cesaryjesus_automoc.cpp"
-  "CMakeFiles/cesaryjesus_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cesaryjesus_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
