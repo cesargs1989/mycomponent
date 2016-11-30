915bc3a0b4f72a4b6d2758661391afd0f40573cd
jechaveza <jechaveza@alumnos.unex.es>
2016-11-30 16:24:41 +0100

    todo

---
+++ supervisor/src/CMakeFiles/supervisor_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "supervisor_automoc.cpp"
+  "CMakeFiles/supervisor_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/supervisor_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
